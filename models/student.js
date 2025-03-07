e87ea37c922fdd0f600820aafcbb5e20ec820b67
dianaNansu2024 <promise24556@gmail.com>
2025-03-07 12:59:25 +0300

    generated the model and modified the student.js file

--- models/student.js
+++ models/student.js
@@ -1,24 +1,30 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
+const { Model, DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
   class Student extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
+    static init(sequelize) {
+      super.init(
+        {
+          name: {
+            type: DataTypes.STRING,
+            allowNull: false
+          },
+          email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+          }
+        },
+        {
+          sequelize,
+          modelName: "Student",
+          tableName: "Students",
+          timestamps: true
+        }
+      );
     }
   }
-  Student.init({
-    name: DataTypes.STRING,
-    email: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Student',
-  });
+
   return Student;
 };
