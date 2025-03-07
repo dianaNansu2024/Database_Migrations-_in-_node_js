2995e70bd02852d6157aa9e86bfe49705bf01bd8
dianaNansu2024 <promise24556@gmail.com>
2025-03-07 13:17:27 +0300

    created the assignments migrations file

---
+++ migrations/20250307101605-create-assignments.js
@@ -0,0 +1,32 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("Assignments", {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      due_date: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("Assignments");
+  }
+};
