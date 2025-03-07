ee4945c6c51bb5f39afc107241ca432fb9a4f90f
dianaNansu2024 <promise24556@gmail.com>
2025-03-07 11:30:11 +0300

    edidted the migration fiel

--- migrations/20250307082210-create-students.js
+++ migrations/20250307082210-create-students.js
@@ -2,21 +2,43 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+  up: async (queryInterface, Sequelize) => {
+      await queryInterface.createTable("Students", {
+          id: {
+              type: Sequelize.INTEGER,
+              autoIncrement: true,
+              primaryKey: true,
+              allowNull: false
+          },
+          name: {
+              type: Sequelize.STRING,
+              allowNull: false
+          },
+          email: {
+              type: Sequelize.STRING,
+              allowNull: false,
+              unique: true
+          },
+          createdAt: {
+              type: Sequelize.DATE,
+              allowNull: false
+          },
+          updatedAt: {
+              type: Sequelize.DATE,
+              allowNull: false
+          }
+      });
   },
+  down: async (queryInterface, Sequelize) => {
+      await queryInterface.dropTable("Students");
+  }
+};
 
-  async down (queryInterface, Sequelize) {
     /**
      * Add reverting commands here.
      *
      * Example:
      * await queryInterface.dropTable('users');
      */
-  }
-};
+  
+
